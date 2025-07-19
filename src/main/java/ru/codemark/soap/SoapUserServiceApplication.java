0ee7e9d76532b47e2dc151cbdca9c161698319da
asharykin <aleksandr-sharykin@mail.ru>
2025-07-19 23:53:20 +0300

    Initial commit

---
+++ src/main/java/ru/codemark/soap/SoapUserServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.codemark.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapUserServiceApplication.class, args);
+    }
+
+}
