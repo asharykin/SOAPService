11c01af5b9d87e86ab2d9d230a0941ae01836735
asharykin <aleksandr-sharykin@mail.ru>
2025-07-25 23:29:37 +0300

    Removed an unused field in Role

--- src/main/java/ru/codemark/soap/entity/Role.java
+++ src/main/java/ru/codemark/soap/entity/Role.java
@@ -4,9 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Entity
 @Table(name = "roles")
 @Getter
@@ -16,7 +13,4 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
-    private Set<User> users = new HashSet<>();
 }
