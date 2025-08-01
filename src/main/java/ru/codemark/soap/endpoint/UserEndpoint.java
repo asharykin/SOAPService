15be8d748d40a837fa2aebcaa18d6d58239685c9
asharykin <aleksandr-sharykin@mail.ru>
2025-08-01 23:54:11 +0300

    Minor changes

--- src/main/java/ru/codemark/soap/endpoint/UserEndpoint.java
+++ src/main/java/ru/codemark/soap/endpoint/UserEndpoint.java
@@ -20,21 +20,25 @@ public class UserEndpoint {
     public GetAllUsersResponse getAllUsers(@RequestPayload GetAllUsersRequest requestDto) {
         return userService.getAllUsers(requestDto);
     }
+
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getUserRequest")
     @ResponsePayload
     public GetUserResponse getUser(@RequestPayload GetUserRequest requestDto) {
         return userService.getUser(requestDto);
     }
+
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "createUserRequest")
     @ResponsePayload
     public CreateUserResponse createUser(@RequestPayload CreateUserRequest requestDto) {
         return userService.createUser(requestDto);
     }
+
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "updateUserRequest")
     @ResponsePayload
     public UpdateUserResponse updateUser(@RequestPayload UpdateUserRequest requestDto) {
         return userService.updateUser(requestDto);
     }
+
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "deleteUserRequest")
     @ResponsePayload
     public DeleteUserResponse deleteUser(@RequestPayload DeleteUserRequest requestDto) {
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,17 +6,17 @@ CREATE TABLE roles (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR NOT NULL UNIQUE
 );
+
 CREATE TABLE users (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR NOT NULL UNIQUE,
     name VARCHAR NOT NULL,
     password VARCHAR NOT NULL
 );
+
 CREATE TABLE users_roles (
      id BIGINT AUTO_INCREMENT PRIMARY KEY,
-     user_id BIGINT NOT NULL,
-     role_id BIGINT NOT NULL,
-     UNIQUE (user_id, role_id),
-     FOREIGN KEY (user_id) REFERENCES users(id),
-     FOREIGN KEY (role_id) REFERENCES roles(id)
+     user_id BIGINT NOT NULL REFERENCES users (id),
+     role_id BIGINT NOT NULL REFERENCES roles (id),
+     UNIQUE (user_id, role_id)
 );
